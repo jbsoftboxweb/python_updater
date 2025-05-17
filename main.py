52ece443540e081e58621ccd7d814c6a09c031a5
JB Softbox <www.jbsoftbox.lk@gmail.com>
2025-05-17 11:38:00 +0530

    main.py
    Source code for main.py file
    
    Happy coding!!

---
+++ main.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.Popen(["python", "updates/updater.py"])
+sys.exit()  # close main app
+
+# add this code into your main file and make sure file type (eg: .py,.exe)
+# You can make this code into def fuction and access it by button & etc..
